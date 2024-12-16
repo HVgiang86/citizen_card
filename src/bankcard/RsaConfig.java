b84a330ac7e6633486fe1295418323c7646e4154
Giang Hoang <hoang.van.giang@sun-asterisk.com>
2024-12-17 00:38:46 +0700

    fix/challenge card

--- src/bankcard/RsaConfig.java
+++ src/bankcard/RsaConfig.java
@@ -13,11 +13,22 @@ public class RsaConfig {
 	//tra ve kich thuoc publickey
 	public static short serializePublicKey(RSAPublicKey key, byte[] buffer, short offset) {
 		//tra ve luy thua cong khai exponentLength co length=3
+		
+		// Structure of x509EncodedKeySpec
+		// [Exponent length][Exponent bytes][Modulus length][Modulus Bytes]
+		//			2 bytes		3 bytes			2 bytes			128 bytes
+		
+		// Set buffer for exponent bytes
 		short exponentLength = key.getExponent(buffer, (short) (offset + 2));
+		
 		//tra ve modun khoa, so bit dich = offset,  modulusLength co length=128
+		
 		short modulusLength = key.getModulus(buffer, (short) (offset + 2 + exponentLength + 2));
+		
+		// Set exponent length
 		Util.setShort(buffer, offset, exponentLength);
+		// Set Modulus length
 		Util.setShort(buffer, (short) (offset + 2 + exponentLength), modulusLength);
-		return (short) (10 + exponentLength + modulusLength);
+		return (short) (4 + exponentLength + modulusLength);
 	}
 }
